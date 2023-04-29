519c1e09bed32e2fe8f00161d2ce3def9d46ed55
Serg <patron.serg@yandex.ru>
2023-04-29 09:45:17 +0300

    git_culbit15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите день недели:");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num==6||num==7)
+Console.WriteLine("да");
+else
+Console.WriteLine("нет");
