72ff31776a993f0f0d79dbfe05af838dafbd92ff
Serg <patron.serg@yandex.ru>
2023-04-29 09:32:55 +0300

    git_culbit

--- task13/Program.cs
+++ task13/Program.cs
@@ -3,3 +3,12 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+while(num>999){
+    num=num/10;
+}
+if(num>99)
+Console.WriteLine($"Третья цифра: {num%10}");
+else
+Console.WriteLine("Третьей цифры нет!");
